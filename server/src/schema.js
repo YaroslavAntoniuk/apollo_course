2f932db0cc6f96f5160009d4e9ccbb4b89263100
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-07 00:00:33 +0300

    Updated module schema & added resolvers

--- server/src/schema.js
+++ server/src/schema.js
@@ -5,6 +5,8 @@ const typeDefs = gql`
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
     track(id: ID!): Track
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module!
   }
 
   type Mutation {
@@ -45,8 +47,14 @@ const typeDefs = gql`
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
     id: ID!
+    "The module's title"
     title: String!
+    "The module's length in minutes"
     length: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
   }
 
   "Author of a complete Track"
